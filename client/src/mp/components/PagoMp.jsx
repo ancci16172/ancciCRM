228404089251773bf3062bfff26097bb1b05b20c
ancci16172 <rodrigovazquez1617@gmail.com>
2024-01-12 15:31:01 -0300

    comienzo a agregar los estilos por status de pago

--- client/src/mp/components/PagoMp.jsx
+++ client/src/mp/components/PagoMp.jsx
@@ -3,11 +3,20 @@
 
 import styles from "./PagoMp.module.css"
 
-export function PagoMp({pago}) {
-    const {id,date_created,payer,transaction_details,status} = pago;
-
+export function PagoMp({ pago }) {
+    const { id, date_created, payer, transaction_details, status } = pago;
+    const stylesPagoContainer = {
+        "approved": styles.pagoAprobado,
+        "inprocess": styles.pagoEnProceso,
+        "inmediation": styles.pagoEnProceso,
+        "rejected": styles.pagoRechazado,
+        "cancelled": styles.pagoRechazado,
+        "refunded": styles.pagoReintegrado,
+        "chargedback": styles.pagoReintegrado,
+    }
+    console.log(status);
     return (
-        <div className={styles.pago}>
+        <div className={styles.pago + " " + (stylesPagoContainer[status] || stylesPagoContainer["approved"])}>
             <div className="flex justify-between">
                 <span>{id}</span>
                 <span className="font-bold">${transaction_details.total_paid_amount}</span>
@@ -20,6 +29,7 @@ export function PagoMp({pago}) {
 
 
 }
+
 
 
 
